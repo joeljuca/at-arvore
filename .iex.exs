69d0997e2e203da9dc025e7a8649cecba9007a34
Joel Jucá <joelwallis@gmail.com>
2023-09-25 15:22:20 -0300

    chore: set up .iex.exs
    See: https://dev.to/joeljuca/introducing-iex-local-exs-2bfk

---
+++ .iex.exs
@@ -0,0 +1,10 @@
+alias Arvore.Entities
+alias Arvore.Entities.Entity
+
+alias ArvoreWeb.EntityController
+alias ArvoreWeb.FallbackController
+alias ArvoreWeb.PageController
+alias ArvoreWeb.Router
+
+# Local dot-iex file (user/environment-specific, Git-ignored)
+import_file_if_available(".iex.local.exs")
