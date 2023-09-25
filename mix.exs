a83632ecc0025e84de074a50ef53bda9578893a0
Joel Jucá <joelwallis@gmail.com>
2023-09-25 16:45:14 -0300

    chore: add dep swiss_schema v0.4.1

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule Arvore.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:swiss_schema, "~> 0.4"},
       {:phoenix, "~> 1.7.2"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
