94f96f100a96b1d0cab98241cfde0a0b9844f3e5
Joel Jucá <joelwallis@gmail.com>
2023-09-27 14:38:00 -0300

    fix: ArvoreWeb.Repo typo in `.iex.exs`

--- .iex.exs
+++ .iex.exs
@@ -1,10 +1,10 @@
 alias Arvore.Entities
 alias Arvore.Entities.Entity
+alias Arvore.Repo
 
 alias ArvoreWeb.EntityController
 alias ArvoreWeb.FallbackController
 alias ArvoreWeb.PageController
-alias ArvoreWeb.Repo
 alias ArvoreWeb.Router
 
 # Local dot-iex file (user/environment-specific, Git-ignored)
